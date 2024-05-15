4e28033eb28d6364bead1489cd4a058aee006345
balababa2143 <balababa2143@gmail.com>
2024-05-15 03:29:11 -0700

    moved KD

--- Src/Common/index.ts
+++ Src/Common/index.ts
@@ -1,5 +1,5 @@
 export * as Helpers from './Helpers'
 export * as Mod from './Mod'
-export { KDInterface as KD, KDInterface } from '../../KinkyDUngeonInterfaceWrapper/Generated/Src'
+export { KDInterface as KD, KDInterface } from '../../KDInterface/Generated/Src'
 export const DEFAULT = 'DEFAULT' as const
 export const RootNamespace = 'KDMod' as const
