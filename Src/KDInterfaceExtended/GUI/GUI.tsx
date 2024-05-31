6dec231cdf08a0a76e23b4c6c0dda09e504962d5
balababa2143 <balababa2143@gmail.com>
2024-05-30 22:09:57 -0700

    fixed UI root resize

--- Src/KDInterfaceExtended/GUI/GUI.tsx
+++ Src/KDInterfaceExtended/GUI/GUI.tsx
@@ -4,20 +4,12 @@ import { createRoot } from 'react-dom/client'
 import { Pixi } from './Pixi'
 import { Html } from './Html'
 
-export function GUI({children} : PropsWithChildren) {
+const GUIRootClass = 'KDInterfaceExtendedGUIRoot' as const
+
+export function GUI({ children }: PropsWithChildren) {
     return (
         <div
-            className='ModUIDomRoot'
-            style={{
-                position: 'absolute',
-                top: '0',
-                left: '50%',
-                transform: 'translate(-50%, 0)',
-                width: 'auto',
-                height: '100%',
-                aspectRatio: '2/1',
-                pointerEvents: 'none'
-            }}
+            className={GUIRootClass}
         >
             <Stage
                 width={PIXIWidth}
@@ -43,10 +35,10 @@ export function GUI({children} : PropsWithChildren) {
                     app.queueResize()
                 }}
             >
-                <Pixi.Out/>
+                <Pixi.Out />
             </Stage>
             {children}
-            <Html.Out/>
+            <Html.Out />
         </div>
     )
 }
@@ -55,5 +47,34 @@ export function CreateModUIRoot() {
     const gameRoot = document.body
     const ModUIRootElm = document.createElement('div')
     gameRoot.appendChild(ModUIRootElm)
+    const css = `
+        div.${GUIRootClass} {
+            position: absolute;
+            pointer-events: none;
+            padding: 0;
+            margin: auto;
+            outline: none;
+            display: block;
+            top: 50%;
+            left: 0;
+            transform: translate(0, -50%);
+            width: 100%;
+            height: auto;
+            aspect-ratio: 2/1;
+        }
+        @media (min-aspect-ratio: 2/1) {
+                div.${GUIRootClass} {
+                    top: 0;
+                    left: 50%;
+                    transform: translate(-50%, 0);
+                    width: auto;
+                    height: 100%;
+                    aspect-ratio: 2/1;
+            }
+        }
+    ` as const
+    const style = document.createElement('style')
+    document.head.appendChild(style)
+    style.appendChild(document.createTextNode(css))
     return createRoot(ModUIRootElm)
 }
